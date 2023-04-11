bb5ffdd711e819e70e2677a24eb6ea72bea3bc7f
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-04-11 00:07:22 -0400

    show scores updated

--- high-score.js
+++ high-score.js
@@ -1,6 +1,7 @@
 let username = $("#username");
 let saveBtn = $("#saveScoreBtn");
 let scoreSpan = $("#score");
+let showScore = $("#showScores");
 
 let scoreKeeper = [];
 
@@ -21,9 +22,17 @@ $(saveBtn).on("click", function (event) {
   scoreKeeper.push(recentScore);
   // this function sorts the array of scoreKeeper and when compares 2 numbers, if b>a, puts b before a. the purpose is to show the usernames in order respected to their score
   scoreKeeper.sort((a, b) => b.score - a.score);
+  scoreKeeper = scoreKeeper.slice(0, 5); // only keeps the highest 5 scores and eliminates the rest
 
   localStorage.setItem("highScore", JSON.stringify(scoreKeeper));
   console.log(scoreKeeper);
-  console.log(JSON.parse(localStorage.getItem("highScore")));
+
+  $(showScore).html(
+    JSON.parse(localStorage.getItem("highScore")).map(
+      (item) =>
+        "<li class='scoreListItem'>" + item.name + " - " + item.score + "</li>"
+    )
+  );
+
   $(username).val("");
 });
